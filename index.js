b5363415b2abb7260daf7c8d77b12b2f9aa1f84b
Yohan Boniface <yb@enix.org>
2014-09-28 23:04:56 +0200

    Fix wrong path after rename

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 exports.Plugin = function (config) {
-    config.addJS('/node_modules/kosmtik-osm-data/front.js');
+    config.addJS('/node_modules/kosmtik-osm-data-overlay/front.js');
     config.addJS('/node_modules/osmtogeojson/osmtogeojson.js');
 };
